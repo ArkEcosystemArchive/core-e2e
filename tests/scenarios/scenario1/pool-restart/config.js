ec95b17c6e179af0750d1f408e1a041e040e6408
Air1 <erwann@ark.io>
2018-11-07 15:48:39 +0400

    test: pool-restart test config with correct blocks

--- tests/scenarios/scenario1/pool-restart/config.js
+++ tests/scenarios/scenario1/pool-restart/config.js
@@ -3,10 +3,10 @@
 module.exports = {
     events: {
         newBlock: {
-            1175: [ '0.transfer-new-wallet.action' ],
-            1177: [ '1.create-tx-remove-node.action' ],
-            1179: [ '2.0.restart-node.action', '2.1.check-tx-1.test' ],
-            1181: [ '3.check-node-pool.test' ]
+            20: [ '0.transfer-new-wallet.action' ],
+            22: [ '1.create-tx-remove-node.action' ],
+            24: [ '2.0.restart-node.action', '2.1.check-tx-1.test' ],
+            26: [ '3.check-node-pool.test' ]
         }
     }
 }

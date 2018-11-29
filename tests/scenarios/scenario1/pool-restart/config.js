443fd74ddf0d62058fb4c11e22373bfa1638c3b6
Air1 <erwann@ark.io>
2018-11-29 17:09:01 +0400

    chore: enable test pool-restart

--- tests/scenarios/scenario1/pool-restart/config.js
+++ tests/scenarios/scenario1/pool-restart/config.js
@@ -19,12 +19,12 @@ Workflow :
 module.exports = {
     events: {
         newBlock: {
-            20: [ '0.transfer-new-wallet.action' ],
-            24: [ '1.create-transactions.action' ],
-            25: [ '2.check-unconfirmed.test' ],
-            26: [ '3.stop-node.action' ],
-            28: [ '4.0.restart-node.action', '4.1.check-tx-1.test' ],
-            38: [ '5.check-node-pool.test' ]
+            10: [ '0.transfer-new-wallet.action' ],
+            14: [ '1.create-transactions.action' ],
+            15: [ '2.check-unconfirmed.test' ],
+            16: [ '3.stop-node.action' ],
+            18: [ '4.0.restart-node.action', '4.1.check-tx-1.test' ],
+            26: [ '5.check-node-pool.test' ]
         }
     }
 }

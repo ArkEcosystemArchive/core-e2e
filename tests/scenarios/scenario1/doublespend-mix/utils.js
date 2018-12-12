f67a4fc04c3d67e82bf7278c1d80a85159925219
Air1 <erwann@ark.io>
2018-12-12 11:43:08 +0400

    test: fix dont generate wallets on the fly in utils.js

--- tests/scenarios/scenario1/doublespend-mix/utils.js
+++ tests/scenarios/scenario1/doublespend-mix/utils.js
@@ -1,42 +1,276 @@
-const { client, crypto } = require('@arkecosystem/crypto')
-const bip39 = require('bip39')
-
-
-const genWallets = () => {
-  const config = require('../../../networks/e2enet/e2enet.json')
-  client.setConfig(config)
-
-  const wallets = []
-  for (let i = 0; i < 5; i++) {
-    const passphrase = bip39.generateMnemonic()
-    const keys = crypto.getKeys(passphrase)
-    const address = crypto.getAddress(keys.publicKey, config.pubKeyHash)
-    wallets.push({ passphrase, address })
-  }
-  
-  return wallets
-}
-
+// new wallets to be used for each double spend combination
 const walletsMix = {
   vote: {
-    delegateRegistration: genWallets(),
-    transfer: genWallets(),
-    secondSignRegistration: genWallets()
+    delegateRegistration: [
+      {
+        passphrase: "same electric inch favorite decide ripple clinic minute grab glove elevator morning",
+        address: "MJTKWXsZVH26WRQjRwJeF5ri9kBx4K3dFJ"
+      },
+      {
+        passphrase: "vintage attract spice swarm heavy square list high tank empty post defense",
+        address: "MQcigxHXG81jUQaKNRgLm6mVsRZWaUE6gq"
+      },
+      {
+        passphrase: "trap magnet eternal wreck sort room imitate original number edit manual aerobic",
+        address: "MDjDpmhG7WvVPKeqFSery2zEcNWm91T8qX"
+      },
+      {
+        passphrase: "civil gather rescue echo border convince mountain diamond allow online fit still",
+        address: "MUdx2FqtJJbLZDVUW6uJqD4J7Hn2rWQjVK"
+      },
+      {
+        passphrase: "space tennis mix damp client twenty island exhaust elder dolphin anger kid",
+        address: "M9bfX2QjNDtJXnAix7k2ipGfJNGid7Y8uQ"
+      }
+    ],
+    transfer: [
+      {
+        passphrase: "honey squirrel sentence pen length where roast much embark raise potato doll",
+        address: "M8ReMW7rxG12WXxBpFXvNe7NqJ7C4NLxZ5"
+      },
+      {
+        passphrase: "sock cradle original receive clay purse accuse recall stock remember artwork giraffe",
+        address: "MMZwhK8gnBrk7pzkpavCBqSds4jSdiBEkj"
+      },
+      {
+        passphrase: "pitch radio genuine nuclear mention bundle include usage bean tragic decrease common",
+        address: "MFq9Yddwi9vCWXvezwckrjscF8YpZyTSvG"
+      },
+      {
+        passphrase: "raw deny nice adult liar foster two census link deer matrix book",
+        address: "MGwkakADWgyhhCyqonr1RuamuxGoAuK9qY"
+      },
+      {
+        passphrase: "science place tribe beef limb ship voice finish mansion bonus prefer awful",
+        address: "MGNWGrJBsWZCp9CXkBY7j2rKDqMWWHEqt5"
+      }
+    ],
+    secondSignRegistration: [
+      {
+        passphrase: "reward item pull cannon cattle curtain wealth region concert require movie rude",
+        address: "MEtZSCdz42eUVjkpAazvTCVrwyW9WU26co"
+      },
+      {
+        passphrase: "drive ketchup select boost amount hand find blue angle antenna age erupt",
+        address: "MEQPSsw7U89fS6a5Hb3tT9Auk9jDWz7qhg"
+      },
+      {
+        passphrase: "celery coast skill trick boring master asthma dress fuel song divide absurd",
+        address: "MSqxxTQGTpVXWvrNUnAJj3J9PcAkUdwW8h"
+      },
+      {
+        passphrase: "cage else session whale ahead security idea inch virtual human whisper addict",
+        address: "MVp9DwGxYiP5SLaEpBEKi2rLCQD1UY2Edk"
+      },
+      {
+        passphrase: "polar achieve exhaust aisle cable crumble idea spike neck tilt course soccer",
+        address: "MLfJqNq8qVuwQUYB8eCAPSA59BcxJyiR5q"
+      }
+    ]
   },
   delegateRegistration: {
-    vote: genWallets(),
-    transfer: genWallets(),
-    secondSignRegistration: genWallets()
+    vote: [
+      {
+        passphrase: "choice snack warfare crush achieve universe salad camp unhappy ask strike orange",
+        address: "M9yU35jHZ8xP7Cqk6ngKSX1pxM4NDgjY4N"
+      },
+      {
+        passphrase: "shock grant horror across lesson hand upper travel hole question rich poet",
+        address: "MBLo14HgDNbYizhcBbnAUzjfKaLinYhsBR"
+      },
+      {
+        passphrase: "repair head diary pluck bulk mother lab deal amateur round release banner",
+        address: "MKk7d5PLBk1PYRvaBs2UeSUzAKYg28K1EE"
+      },
+      {
+        passphrase: "measure toward pipe hybrid toilet tonight path special minimum cannon chase decade",
+        address: "MDg7PkErYZ2AgTLLBcf9yTznU38AxDB7qd"
+      },
+      {
+        passphrase: "illness desert lunar old usage member report river prefer sample slight athlete",
+        address: "MTHvXgw6azXdtQpnqaBsr7o3cpioXub8Tb"
+      }
+    ],
+    transfer: [
+      {
+        passphrase: "lady cheese crisp analyst apart junior coyote tackle online together worry road",
+        address: "MUE6bUWwwM8pownH2EUQQLY1Drx5b7DP4z"
+      },
+      {
+        passphrase: "coconut spawn planet drill soft festival exercise tube video acid warfare lecture",
+        address: "MEkuKqThE8Yjmxv2L39pjgzuEMrQYdgJQh"
+      },
+      {
+        passphrase: "mean major bone frozen chuckle access obey plastic fall brain style crawl",
+        address: "MDYZwSgSV6BvMqv9Kf26MWtEXVB3wmj2VL"
+      },
+      {
+        passphrase: "hurdle enforce defense topple heart raccoon already tree tree sad asset place",
+        address: "MByNDv93EkBJmASRbT6FVSweAU96jp4DmE"
+      },
+      {
+        passphrase: "abandon silver situate happy only nothing arrest mention before smooth solve reason",
+        address: "MEspEHoPFbvRK9xRjSone4xwhEgjhojQJC"
+      }
+    ],
+    secondSignRegistration: [
+      {
+        passphrase: "flock square social critic gallery nominee display gift tank ready banana degree",
+        address: "MLXic3jgYbssTKqLVUaBnSpUurHiNdXaXF"
+      },
+      {
+        passphrase: "hammer inspire bronze lamp chunk wait maximum pottery jelly silent prosper lazy",
+        address: "MLcR3sSv1M52dd8KeKq4n8zmnCfMLzrhtR"
+      },
+      {
+        passphrase: "then payment degree wise antenna diamond modify tooth able silly remember taste",
+        address: "MB8HMemv6qZ8Z5pKA2TjKSYPf7Rk6AqNJm"
+      },
+      {
+        passphrase: "below rude anger help aunt master evil industry lock gossip toward reflect",
+        address: "MVGejUGD48L1wTrkjnLXofQdWEfwNCyat1"
+      },
+      {
+        passphrase: "you erosion symbol bamboo blouse bread journey discover moment chronic slogan section",
+        address: "MLASnnBT4JtFMok2fhRYMsGsXFXGceiFnB"
+      }
+    ]
   },
   transfer: {
-    vote: genWallets(),
-    delegateRegistration: genWallets(),
-    secondSignRegistration: genWallets()
+    vote: [
+      {
+        passphrase: "tower grow before valve index fever trip fee venue blood erase relief",
+        address: "MPvn5Ttzvjqk1uPqAkzXSSinD5MC8kMvDb"
+      },
+      {
+        passphrase: "lift museum educate only bonus into clarify field belt borrow page divorce",
+        address: "MCXGMvKZecwkkUrxEpWYrg2mP7jBfdVjUz"
+      },
+      {
+        passphrase: "marriage theme tilt episode spice hurry tomorrow small work drama mandate tape",
+        address: "MUAZkFLBYSUezXuJrtZRFjGXiNesjRwzs3"
+      },
+      {
+        passphrase: "furnace year van predict height drill question harsh ordinary joke patrol stay",
+        address: "M82R5Pn857CkuTGBK86kNpWUeMeDCezBJ6"
+      },
+      {
+        passphrase: "text cream pride merry soldier fall water defy slot dial motion mistake",
+        address: "MT8SkQb8Ef4nRzjkyMhEvY5nptr97B39jJ"
+      }
+    ],
+    delegateRegistration: [
+      {
+        passphrase: "ozone exist else estate naive catalog anxiety tell hole hockey idea hill",
+        address: "MLmB1Aoy4R649VXYEcNizNhB9EXG1wb4QE"
+      },
+      {
+        passphrase: "uncle rate rotate shock rose large spider captain person illegal hammer sure",
+        address: "MCa17heR8iBx3yZh4XKPiQQnq5TwjbGfNT"
+      },
+      {
+        passphrase: "mouse ignore pull retreat vague logic still zoo suggest burger push renew",
+        address: "MHavYpLyzoGR1TTFV6dHQZpHy2BtsdhjjG"
+      },
+      {
+        passphrase: "shield tag cup chalk cinnamon harvest vibrant mammal sing notice cargo victory",
+        address: "MKW2VPe6vp4KF5bAjM1tx7nx9Kb3AwKHXf"
+      },
+      {
+        passphrase: "lawsuit cousin tell gospel topple urban praise gallery junior face skate bless",
+        address: "M8W1kgf2Swc7zDWQAAZmbtVmND32TXWbVp"
+      }
+    ],
+    secondSignRegistration: [
+      {
+        passphrase: "grape circle joke face stairs record sign finger manage zero prevent kick",
+        address: "MGnnN3VNF2cZDzSZaQL1DVa6iddZimN9EE"
+      },
+      {
+        passphrase: "seat lava cancel coconut finish harsh pattern feature scheme tower remember lounge",
+        address: "MS8pY1ZqvFosMvLGyKEMuXeVjPFhLmxzX2"
+      },
+      {
+        passphrase: "sword current garbage choice slab gesture puppy build face fatigue filter door",
+        address: "MF98wtx9vvqG8kzxeu5CK23uVPX7Mwfna6"
+      },
+      {
+        passphrase: "gorilla sad final agree fabric wrestle rice quit ivory library rather puppy",
+        address: "MDrqmUyqrWdEyyGh5K2qof9UUgumM9Qbue"
+      },
+      {
+        passphrase: "blur quantum foil effort three educate manual toss image team delay brother",
+        address: "MRpDtE1WevfJ41J8mK57pjCm6f8YZsTwJ1"
+      }
+    ]
   },
   secondSignRegistration: {
-    vote: genWallets(),
-    delegateRegistration: genWallets(),
-    transfer: genWallets()
+    vote: [
+      {
+        passphrase: "genuine feel impact any melt lava atom couch manage define once beach",
+        address: "M93nxubhV9psvWftXnLkbXpDsuAFu8sTTY"
+      },
+      {
+        passphrase: "canyon organ slim palm age okay horse camp permit matrix empty airport",
+        address: "MJEBKb36kArDFTirnCAUr6YJ1Vt8ERTT1u"
+      },
+      {
+        passphrase: "marriage record convince arena reason flash slab trick trigger thank secret woman",
+        address: "MPu4VtwxeRYLwnKRAbxZhpC3WvHaZB3b2y"
+      },
+      {
+        passphrase: "prison firm comfort property broken story skate exchange jump patch giggle device",
+        address: "M8aCNBJUXnwYHn3ttP9nrCs7yC7RkmNfWk"
+      },
+      {
+        passphrase: "dwarf salmon glance raw venture try major deposit fruit rigid blast mushroom",
+        address: "M8aVpdpZnLm2oZ2vGo5sT8DN55VQuKnM9W"
+      }
+    ],
+    delegateRegistration: [
+      {
+        passphrase: "shiver child degree topple process fragile echo mind sail fuel machine fall",
+        address: "MMVVcfyktaDsMNiE24c8jVte5EUVj2Vz9D"
+      },
+      {
+        passphrase: "scorpion slot embrace traffic advice gentle pluck fossil super thing resource theory",
+        address: "M8NVA8quZ1vU9JJjo3x5C7t4ghPGfHaPKw"
+      },
+      {
+        passphrase: "dutch mask basic castle impulse impact tool yellow woman camera cute close",
+        address: "MJrHGJZv41JBhmTWP8cPseZk2gBQtnfyR3"
+      },
+      {
+        passphrase: "ring slab nothing damage dwarf citizen away coach cable fee silver net",
+        address: "MQQ4PE1i68UCLZy8QDr16kYMu861yndSFE"
+      },
+      {
+        passphrase: "blue cherry slice service where corn veteran zebra love iron blossom disease",
+        address: "MHTSWbA5h8QsbWgGcVKweAusExPVqb1GDJ"
+      }
+    ],
+    transfer: [
+      {
+        passphrase: "dance core deal brush rude social pride bomb proud effort cargo crouch",
+        address: "MSthcZ1ixKE4B6f2VjmxBo5TUo5P84y2hz"
+      },
+      {
+        passphrase: "maid major say relief page impact witness grow pulp spawn hen cook",
+        address: "M9PwYSwBpU4Ry4piQzyTXkgpMJQRbwnrZq"
+      },
+      {
+        passphrase: "network high rebuild accident scheme shoulder time hello evil diamond biology lecture",
+        address: "MPW16xQrM3yt8u6erQkDXwAq9oMQ9bhFfQ"
+      },
+      {
+        passphrase: "office angle rural train basket taxi float drop panther boil canal asset",
+        address: "MEuNDpc8q9V8czTaQeNRdo3bp9TRfCbCyz"
+      },
+      {
+        passphrase: "tape myself tortoise now clap ahead menu foot decorate sentence empty plug",
+        address: "MPbkaupeThFxr5ug5DZtAbHZeza8jq2vXR"
+      }
+    ]
   }
 }
 

a92dc51bca1222a7770c765811eac35ec18adc9e
Air1 <erwann@ark.io>
2018-12-21 09:32:15 +0100

    fix: wrong import

--- tests/networks/e2enet/crypto/index.ts
+++ tests/networks/e2enet/crypto/index.ts
@@ -1,6 +1,6 @@
 import { devnet } from "./devnet";
 import { mainnet } from "./mainnet";
 import { testnet } from "./testnet";
-import { e2enet } from "./testnet";
+import { e2enet } from "./e2enet";
 
 export { devnet, mainnet, testnet, e2enet };

b9b75f611be002d31c250017a532fb5b69265348
Air1 <erwann@ark.io>
2019-01-02 14:45:01 +0400

    fix: e2enet config path

--- lib/config/docker/ark-network-start.sh
+++ lib/config/docker/ark-network-start.sh
@@ -1,3 +1,3 @@
 echo "Starting ark --network-start" >> output.log
-ARK_LOG_FILE=ark.log node ./packages/core/dist/index.js start --config packages/core/lib/config/e2enet --network e2enet --network-start >> output.log 2> errors.log
+ARK_LOG_FILE=ark.log node ./packages/core/dist/index.js start --config packages/core/src/config/e2enet --network e2enet --network-start >> output.log 2> errors.log
 echo "Started ark --network-start" >> output.log
--- lib/config/docker/ark.sh
+++ lib/config/docker/ark.sh
@@ -1,5 +1,5 @@
 echo "Starting ark" >> output.log
-ARK_LOG_FILE=ark.log node ./packages/core/dist/index.js start --config packages/core/lib/config/e2enet --network e2enet >> output.log 2> errors.log &
+ARK_LOG_FILE=ark.log node ./packages/core/dist/index.js start --config packages/core/src/config/e2enet --network e2enet >> output.log 2> errors.log &
 echo kill -2 $! > killpid.sh
 pwd >> output.log
 cat killpid.sh >> output.log
